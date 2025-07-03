e93ebdfa0cd3a1a1f5b602511e87c5f893b496ec
elefsaria <mitraponsel5828@gmail.com>
2025-07-04 01:51:28 +0700

    Create dummyNFTs.js

---
+++ src/data/dummyNFTs.js
@@ -0,0 +1,18 @@
+const dummyNFTs = [
+  {
+    id: "nft-1",
+    name: "Cyber Ape #001",
+    image: "https://via.placeholder.com/300x300.png?text=Ape+001",
+    description: "The first Cyber Ape in the collection.",
+    price: null,
+  },
+  {
+    id: "nft-2",
+    name: "Cyber Ape #002",
+    image: "https://via.placeholder.com/300x300.png?text=Ape+002",
+    description: "The second Cyber Ape in the collection.",
+    price: null,
+  },
+];
+
+export default dummyNFTs;
